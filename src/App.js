ecd268b4533ff6d085ce8c95ad0e81a736895374
okey <obilekekachi@gmail.com>
2023-03-22 20:12:46 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Try out</h1>
+       <h1>Hope this works</h1>
+    </div>
+  );
+}
+
+export default App;
